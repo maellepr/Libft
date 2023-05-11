c47425e6bf607ad24f29ca87346312803d3b6066
maellepr <69607927+maellepr@users.noreply.github.com>
2023-05-11 17:26:31 +0200

    Add files via upload

---
+++ ft_lstnew.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapoirie <mapoirie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/11 13:38:51 by mapoirie          #+#    #+#             */
+/*   Updated: 2023/05/11 17:25:47 by mapoirie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*list;
+
+	list = malloc(sizeof(t_list));
+	list->content = content;
+	list->next = NULL;
+	return(list);
+}
